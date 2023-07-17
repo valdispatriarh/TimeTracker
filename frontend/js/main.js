0e5206f17d5a6ee088b635da7ac787aebd509611
Vlad <valdispatriarh@gmail.com>
2023-07-17 11:52:06 +0200

    feat: add class Task and Timer

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -26,6 +26,7 @@ function addBodyCentrumColumn () {
     <div class = "centrum-column">
         <div class = "time"></div>
         <div class = "date"></div>
+        <div class = "add-task">Add task</div>
         <div class = "textareas-and-button">
             <div class = "textarea-task"><input type = 'text'></div>
             <div class = "textarea-time"><input type = 'time'></div>
@@ -44,11 +45,12 @@ function addBodyLeftColumn () {
         <div class = "main-goals">
         <h4>Task list</h4>
           <ul>
-            <li>task1
-            <div class = "player">
+            <li id = 'box1'> task1
+            <div class = "player" >
                 <div class = "btn play"><img src='../assets/images/play.png' alt=play class = play-btn-img></div>
                 <div class = "btn pause"><img src='../assets/images/pause.png' alt=pause class = pause-btn-img></div>
-                <div class = "btn done"><img src='../assets/images/done.png' alt=play class = done-btn-img></div>
+                <div class = "btn done"><img src='../assets/images/done.png' alt=play class = done-btn-img>
+                </div><div id="timer1">60</div>
             </div>
             </li>
             <li>task2
@@ -56,6 +58,7 @@ function addBodyLeftColumn () {
                 <div class = "btn play"><img src='../assets/images/play.png' alt=play class = play-btn-img></div>
                 <div class = "btn pause"><img src='../assets/images/pause.png' alt=pause class = pause-btn-img></div>
                 <div class = "btn done"><img src='../assets/images/done.png' alt=play class = done-btn-img></div>
+                
             </div>
             </li>
             <li>task3
@@ -79,11 +82,45 @@ function addBodyLeftColumn () {
 }
 addBodyLeftColumn();
 
+function addBodyRightColumn () {
+  const container = document.querySelector('.container');
+  const rightColumn = document.createElement('div');
+  rightColumn.classList.add('container');
+  rightColumn.innerHTML = `
+    <div class = "right-column">
+        <div class = "main-goals">
+        <h4>Task list</h4>
+          <ul>
+            <li>task1
+            <div class = "player">
+                <div class = "btn play"><img src='../assets/images/play.png' alt=play class = play-btn-img></div>
+                <div class = "btn pause"><img src='../assets/images/pause.png' alt=pause class = pause-btn-img></div>
+                <div class = "btn done"><img src='../assets/images/done.png' alt=play class = done-btn-img>
+                </div><div id="timer1">60</div>
+            </div>
+            </li>
+            <li>task2
+            <div class = "player">
+                <div class = "btn play"><img src='../assets/images/play.png' alt=play class = play-btn-img></div>
+                <div class = "btn pause"><img src='../assets/images/pause.png' alt=pause class = pause-btn-img></div>
+                <div class = "btn done"><img src='../assets/images/done.png' alt=play class = done-btn-img></div>
+                
+            </div>
+            </li>
+            </li>
+          </ul>
+        </div>
+    </div>`;
+  container.appendChild(rightColumn);
+}
+addBodyRightColumn();
+
+setInterval(showTimeAndDate, 1000);
 function showTimeAndDate () {
   const tagTime = document.querySelector('.time');
+  const tagDate = document.querySelector('.date');
   const date = new Date();
   tagTime.textContent = `${date.toLocaleTimeString()}`;
-  const tagDate = document.querySelector('.date');
   const options = {
     weekday: 'long',
     year: 'numeric',
@@ -91,15 +128,14 @@ function showTimeAndDate () {
     day: 'numeric'
   };
   tagDate.textContent = `${date.toLocaleDateString('en-US', options)}`;
-  setTimeout(showTimeAndDate, 1000);
 }
-showTimeAndDate();
 
 function showTextareaAndButton () {
   const tagTextareaAndButton = document.querySelector('.textareas-and-button');
-  tagTextareaAndButton.innerHTML = `<div class = "textarea-task"><input type = 'text'></div>
-                                          <div class = "textarea-time"><input type = 'time'></div>
-                                          <div class = "button-submit"><input type = 'button' value = '+'></div>`;
+  tagTextareaAndButton.innerHTML = `<div class = "textarea-task"><input type = 'text' ></div>
+                                          <div class = "textarea-time"><input type = 'text' id = 'time'></div>
+                                          <div>min</div>
+                                          <div class = "button-submit"><input type = 'button' value = '+' id = 'button'></div>`;
 }
 showTextareaAndButton();
 
@@ -134,3 +170,137 @@ StartTimer();
 function PressButtonSubmitAnswer () {
 }
 PressButtonSubmitAnswer();
+
+// function timer () {
+//   const timerInput = document.getElementById('time'); // Берём строку
+//   const buttonRun = document.getElementById('button');// Берём кнопку запуска
+//   const timerShow = document.getElementById('timer'); // Берём блок для показа времени
+
+//   buttonRun.addEventListener('click', function () {
+//     let timeMinut = parseInt(timerInput.value) * 60;
+
+//     const goodTimer = setInterval(function () {
+//       const seconds = timeMinut % 60; // Получаем секунды
+//       const min = timeMinut / 60; // Получаем минуты
+//       console.log(min);
+//       const hour = timeMinut / 60 / 60 % 60; // Получаем часы
+//       // Условие если время закончилось то...
+//       if (timeMinut <= 0) {
+//         // Таймер удаляется
+//         clearInterval(goodTimer);
+//         // Выводит сообщение что время закончилось
+//         alert('Время закончилось');
+//       } else { // Иначе
+//         // Создаём строку с выводом времени
+//         const strTimer = `${Math.trunc(hour)}:${Math.trunc(min)}:${seconds}`;
+//         // Выводим строку в блок для показа таймера
+//         timerShow.innerHTML = strTimer;
+//       }
+//       --timeMinut; // Уменьшаем таймер
+//     }, 1000);
+//   });
+// }
+// timer();
+const timerInput = document.getElementById('time'); // Берём строку
+const buttonRun = document.getElementById('button');// Берём кнопку запуска
+const timerShow1 = document.getElementById('timer1'); // Берём блок для показа времени
+const timerShow2 = document.getElementById('timer2');
+
+const timeMinut = 0;
+// class Timer {
+//   constructor (selector) {
+//     this.$el = document.getElementById(selector);
+//   }
+
+//   start () {
+//     buttonRun.addEventListener('click', function () {
+//       let timeMinut = parseInt(timerInput.value) * 60;
+//       const goodTimer = setInterval(function () {
+//         const seconds = timeMinut % 60; // Получаем секунды
+//         const min = timeMinut / 60; // Получаем минуты
+//         const hour = timeMinut / 60 / 60 % 60; // Получаем часы
+//         // Условие если время закончилось то...
+//         if (timeMinut <= 0) {
+//         // Таймер удаляется
+//           clearInterval(goodTimer);
+//           // Выводит сообщение что время закончилось
+//           alert('Время закончилось');
+//         } else { // Иначе
+//         // Создаём строку с выводом времени
+//           const strTimer = `${Math.trunc(hour)}:${Math.trunc(min)}:${seconds}`;
+//           // Выводим строку в блок для показа таймера
+//           this.$el.innerHTML = strTimer;
+//         }
+//         --timeMinut; // Уменьшаем таймер
+//       }, 1000);
+//     });
+//   }
+// }
+// class TimerTask extends Timer {
+//   constructor (options) {
+//     super(options.selector);
+//   }
+// }
+// const timerTask1 = new TimerTask({ selector: 'timer1' });
+// timerTask1.start();
+
+class Timer {
+  start () {
+    buttonRun.addEventListener('click', function () {
+      let timeMinut = parseInt(timerInput.value) * 60;
+      const goodTimer = setInterval(function () {
+        const seconds = timeMinut % 60; // Получаем секунды
+        const min = timeMinut / 60; // Получаем минуты
+        const hour = timeMinut / 60 / 60 % 60; // Получаем часы
+        // Условие если время закончилось то...
+        if (timeMinut <= 0) {
+        // Таймер удаляется
+          clearInterval(goodTimer);
+          // Выводит сообщение что время закончилось
+          alert('Время закончилось');
+        } else { // Иначе
+        // Создаём строку с выводом времени
+          const strTimer = `${Math.trunc(hour)}:${Math.trunc(min)}:${seconds}`;
+          // Выводим строку в блок для показа таймера
+          timerShow1.innerHTML = strTimer;
+        }
+        --timeMinut; // Уменьшаем таймер
+      }, 1000);
+    });
+  }
+}
+
+const timerTask1 = new Timer();
+timerTask1.start();
+
+class Task {
+  constructor (selector) {
+    this.$el = document.querySelector(selector);
+    console.log(this.$el);
+  }
+
+  hide () {
+    this.$el.style.display = 'none';
+  }
+
+  show () {
+    this.$el.style.display = 'block';
+  }
+}
+
+class TaskList extends Task {
+  constructor (options) {
+    super(options.selector);
+    this.$el.style.width = this.$el.style.height = options.size + 'px';
+    this.$el.style.background = options.color;
+  }
+}
+const TaskList1 = new TaskList({
+  selector: '#box1',
+  size: 200,
+  color: 'red'
+});
+
+buttonRun.addEventListener('click', function () {
+  TaskList1.show();
+});
---
+++ frontend/js/testClass.js
@@ -0,0 +1,110 @@
+// const animal = {
+//   name: 'Animal',
+//   age: 14,
+//   hasTail: true
+// };
+class Animal {
+  static type = 'AnimaL';
+  constructor (options) {
+    this.name = options.name;
+    this.age = options.age;
+    this.hasTail = options.hasTail;
+  }
+
+  voice () {
+    console.log('I am animal!');
+  }
+}
+
+const animal = new Animal({
+  name: 'Animal',
+  age: 45,
+  hasTail: false
+});
+
+class Cat extends Animal {
+  constructor (options) {
+    super(options);
+    this.color = options.color;
+  }
+
+  voice () {
+    super.voice();
+    console.log('I am cat');
+  }
+
+  get catInfo () {
+    return this.age * 2;
+  }
+
+  set catInfo (newAge) {
+    this.age = newAge;
+  }
+}
+const cat = new Cat({
+  name: 'Pussi',
+  color: 'red',
+  age: 3
+}
+);
+class Component {
+  constructor (selector) {
+    this.$el = document.querySelector(selector);
+  }
+
+  hide () {
+    this.$el.style.display = 'none';
+  }
+
+  show () {
+    this.$el.style.display = 'block';
+  }
+}
+
+class Box extends Component {
+  constructor (options) {
+    super(options.selector);
+    this.$el.style.width = this.$el.style.height = options.size + 'px';
+    this.$el.style.background = options.color;
+  }
+}
+const box1 = new Box({
+  selector: '#box1',
+  size: 100,
+  color: 'red'
+}
+);
+class Round extends Box {
+  constructor (options) {
+    super(options);
+    this.$el.style.borderRadius = '50%';
+  }
+}
+const c = new Round({
+  selector: '#r',
+  size: 100,
+  color: 'red'
+}
+);
+
+5 * 2 + 7 * 2;
+10 * 2 + 6 * 2;
+3 * 2 + 4 * 2;
+
+function pir (b, c) {
+  return c * 3 + b * 2;
+}
+pir(5, 7);
+function pir2 (figure) {
+  return figure.height * 2 + figure.width * 2;
+}
+const myPar = {
+  height: 5,
+  width: 7
+};
+pir(myPar.height, myPar.width);
+pir2(myPar);
+pir2({
+  height: 100,
+  width: 100
+});
