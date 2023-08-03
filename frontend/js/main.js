0255336e41969a1d66ec1c7872fa21e9095be4ed
Vlad <valdispatriarh@gmail.com>
2023-08-03 16:32:39 +0200

    test

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -38,107 +38,99 @@ function showTimeAndDate () {
   };
   tagDate.textContent = `${date.toLocaleDateString('en-US', options)}`;
 }
-function ShowQuestion () {
-  const tagTextareaAndButton = document.querySelector('.textareas-and-button');
-  tagTextareaAndButton.innerHTML = `Ты уложился в отведенное для себя время?
-                                     <div><input type = 'button' value='yes'></div>
-                                     <div><input type = 'button' value='no'></div>
-                                     <div><input type = 'text' value='Почему?'></div>
-                                     <div class = "button-submit"><input type = 'button' value = '+'></div>`;
-  let answerYes;
-  let answerWhy;
-}
+// function ShowQuestion () {
+//   const tagTextareaAndButton = document.querySelector('.textareas-and-button');
+//   tagTextareaAndButton.innerHTML = `Ты уложился в отведенное для себя время?
+//                                      <div><input type = 'button' value='yes'></div>
+//                                      <div><input type = 'button' value='no'></div>
+//                                      <div><input type = 'text' value='Почему?'></div>
+//                                      <div class = "button-submit"><input type = 'button' value = '+'></div>`;
+//   let answerYes;
+//   let answerWhy;
+// }
 //     ShowQuestion()
 
-let timeMinut = 0;
-let goodTimer = 0;
-let timeDataToPlayOnPause = 0;
 class Timer {
-  constructor (selector) {
-    this.$show = document.querySelector(selector);
+  constructor (el) {
+    this.$show = el;
+    this.timeSeconds = 0;
+    this.timerInterval = null;
+    this.timeDataToPlayOnPause = 0;
   }
 
   start () {
     const timerInput = document.getElementById('time');
-    timeMinut = parseInt(timerInput.value) * 60;
-    goodTimer = setInterval(() => {
-      const seconds = timeMinut % 60;
-      const min = timeMinut / 60 % 60;
-      const hour = timeMinut / 60 / 60 % 60;
-      if (timeMinut <= 0) {
-        clearInterval(goodTimer);
+    this.timeSeconds = parseInt(timerInput.value) * 60;
+    this.timerInterval = setInterval(() => {
+      if (this.timeSeconds <= 0) {
+        clearInterval(this.timerInterval);
+        this.timerInterval = null;
         alert('Время закончилось');
       } else {
-        const strTimer = `${Math.trunc(hour)}:${Math.trunc(min)}:${seconds}`;
-        this.$show.innerHTML = strTimer;
+        --this.timeSeconds;
+        this.updateDisplay();
       }
-      --timeMinut;
-      timeDataToPlayOnPause = timeMinut;/// ////////////////////////////////////
+
+      this.timeDataToPlayOnPause = this.timeSeconds;
     }, 1000);
   }
 
   pause () {
-    clearInterval(goodTimer);
-    // const timeDataToPlayOnPause = timeMinut;
+    clearInterval(this.timerInterval);
+    this.timerInterval = null;
   }
 
-  startOnPause () {
-    timeMinut = timeDataToPlayOnPause;/// //////////////////
-    goodTimer = setInterval(function () {
-      const seconds = timeMinut % 60;
-      const min = timeMinut / 60 % 60;
-      const hour = timeMinut / 60 / 60 % 60;
-      if (timeMinut <= 0) {
-        clearInterval(goodTimer);
-        alert('Время закончилось');
-      } else {
-        const strTimer = `${Math.trunc(hour)}:${Math.trunc(min)}:${seconds}`;
-        this.$show.innerHTML = strTimer;
-      }
-      --timeMinut;
-      timeDataToPlayOnPause = timeMinut;
-    }, 1000);
+  resume () {
+    if (this.timerInterval === null) {
+      this.timeSeconds = this.timeDataToPlayOnPause;
+      this.timerInterval = setInterval(() => {
+        console.log(1, this);
+        if (this.timeSeconds <= 0) {
+          clearInterval(this.timerInterval);
+          this.timerInterval = null;
+          alert('Время закончилось');
+        } else {
+          console.log(2, this);
+          --this.timeSeconds;
+          this.updateDisplay();
+        }
+
+        this.timeDataToPlayOnPause = this.timeSeconds;
+      }, 1000);
+    }
+  }
+
+  updateDisplay () {
+    const seconds = this.timeSeconds % 60;
+    const minutes = Math.trunc(this.timeSeconds / 60) % 60;
+    const hours = Math.trunc(this.timeSeconds / 3600);
+    const strTimer = `${hours}:${minutes}:${seconds}`;
+    this.$show.innerHTML = strTimer;
   }
 }
-// class Task {
-//   constructor (selector) {
-//     this.$el = document.querySelector(selector);
-//   }
-
-//   hide () {
-//     this.$el.style.display = 'none';
-//   }
-
-//   show () {
-//     this.$el.style.display = 'block';
-//   }
-// }
-
-// class TaskList extends Task {
-//   constructor (options) {
-//     super(options.selector);
-//     this.$el.style.width = this.$el.style.height = options.size + 'px';
-//     this.$el.style.background = options.color;
-//   }
-// }
-// const TaskList1 = new TaskList({
-//   selector: '#box1',
-//   size: 200,
-//   color: 'red'
-// });
+const timers = [];
 
 const buttonRun = document.getElementById('button');
 
 buttonRun.addEventListener('click', function () {
   const ul = document.getElementById('dailyTimerList');
   const childrenCount = ul.children.length;
-  const newLiEl = createrTimerEl(childrenCount);
+  const [newLiEl, resumeBtn, pauseBtn, countEl] = createTimerEl(childrenCount);/// ////////////////////////////
   ul.appendChild(newLiEl);
-  const timer1 = new Timer(`#timer-${childrenCount}-count`);
-  timer1.start();
+
+  const timer = new Timer(countEl);
+  resumeBtn.addEventListener('click', () => timer.resume());
+  pauseBtn.addEventListener('click', () => timer.pause());
+  timers.push(timer);
+  console.log(timers);
+  timer.start();
 });
 
-function createrTimerEl (id) {
+function createTimerEl (id) {
+  const resumeBtnId = `timer-${id}-resume`;
+  const pauseBtnId = `ttimer-${id}-pause`;
+  const countId = `timer-${id}-count`;
+
   const li = document.createElement('li');
   li.setAttribute('id', `timer-${id}`);
   li.innerHTML = `
@@ -147,19 +139,23 @@ function createrTimerEl (id) {
         <div class = "btn play">
           <img src='../assets/images/play.png' 
                alt=play
-               id = 'timer-${id}-play' 
-               class = play-btn-img 
-               onclick="() => { timerTask1.startOnPause(); }>
+               id = '${resumeBtnId}' 
+               class = play-btn-img>
         </div>
         <div class = "btn pause">
           <img src='../assets/images/pause.png' 
-               alt=pause id = 'timer-${id}-pause' 
-               class = pause-btn-img 
-               onclick="alert('pause')">
+               alt=pause id = '${pauseBtnId}' 
+               class = pause-btn-img">
         </div>
         <div class = "btn done"><img src='../assets/images/done.png' alt=play class = done-btn-img></div>
     </div>
-    <div  title = 'nostart' class ="timers1"  id = 'timer-${id}-count' ></div>
+    <div  title = 'nostart' class ="timers1"  id = '${countId}' ></div>
   `;
-  return li;
+
+  const resumeBtn = li.querySelector(`#${resumeBtnId}`);
+  const pauseBtn = li.querySelector(`#${pauseBtnId}`);
+  const countEl = li.querySelector(`#${countId}`);
+
+  return [li, resumeBtn, pauseBtn, countEl];
 }
+// test
